3264d3318c7b35460b75d0810f8619026f70aaab
klonnage <vinc.brdionneau@gmail.com>
2015-05-02 18:57:25 +0200

    ajout de l'affichage du timer, et de la regle qui permet de stopper le jeu au bout de 20 secondes

--- Projet-Stage/labyrinthe/jeu/labyrinthe.py
+++ Projet-Stage/labyrinthe/jeu/labyrinthe.py
@@ -1,14 +1,5 @@
 #! /usr/bin/env python2
 
-#Variables:
-
-carte = list()
-fin = list()
-perso = list()
-perso_graphique = list()
-fenetre = None
-game_continue = bool()
-event = None
 
 #Debut:
 import pygame, os
@@ -20,72 +11,116 @@ from constantes import *
 #initialise l'api
 initialiser()
 
-#creer la fenetre
-fenetre = creer_fenetre(6 * LARGEUR, 7 * LARGEUR)
+#creer la police 'police_basique'
+police_basique = creer_police(CHEMIN + 'police_basique.ttf', 55)
 
 #initialise la carte
-carte = creer_carte()
+carte, perso, fin = creer_jeu()
 
-#creer le perso
-perso = creer_perso()
+#creer la fenetre
+fenetre = creer_fenetre(len(carte[0]) * LARGEUR, len(carte) * LARGEUR)
 
-#creer la fin
-fin = creer_fin()
+#affiche la carte et le perso
+dessiner_carte(fenetre,carte,fin)
 
 #condition pour que le jeu continue
-game_continue = True
+jeu_en_cours = True
+
+#temps de depart pour le jeu
+temps_debut = obtenir_temps()
 
 #tant que le jeu est en cours
-while game_continue:
-	#affiche la carte et le perso
-	dessiner_tout(fenetre,carte,perso,fin)
+while jeu_en_cours:
+	#calcul de la difference de temps a afficher
+	temps_actuel = obtenir_temps()
+	temps = temps_actuel - temps_debut
 	
-	#on attend un evenement
-	event = pygame.event.wait()
+	#permet d'afficher le timer
+	afficher_temps(temps, police_basique, bleu)
 	
-	#On regarde le type de l'evenement
-	#si l'utilisateur a demande de fermer la fenetre
-	if event.type == QUIT:
-		game_continue = False
-	#si l'utilisateur appuie sur une touche
-	elif event.type == KEYDOWN:
-		#s'il appuie sur la touche 'Droite'
-		if event.key == K_RIGHT:
-			#fixe la direction du perso a la valeur de 'DROITE'
-			perso[2] = DROITE
+	#synchronise les actions (ex: touche enfoncee) avec l'ordinateur
+	synchroniser_actions()
+	
+	#on regarde les touches sur lesquelles l'utilisateur appuient
+	clavier = obtenir_touche()
+	
+	#indique que le personnage se deplace
+	deplace = False
 			
-			#deplace le perso sur la carte
-			deplacer_perso(carte,perso)
-		#s'il appuie sur la touche 'Gauche'
-		elif event.key == K_LEFT:
-			#fixe la direction du perso a la valeur de 'GAUCHE'
-			perso[2] = GAUCHE
-			
-			#deplace le perso sur la carte
-			deplacer_perso(carte,perso)
-		#s'il appuie sur la touche 'Bas'
-		elif event.key == K_DOWN:
-			#fixe la direction du perso a la valeur de 'BAS'
-			perso[2] = BAS
-			
-			#deplace le perso sur la carte
-			deplacer_perso(carte,perso)
-		#s'il appuie sur la touche 'Haut'
-		elif event.key == K_UP:
-			#fixe la direction du perso a la valeur de 'HAUT'
-			perso[2] = HAUT
-			
-			#deplace le perso sur la carte
-			deplacer_perso(carte,perso)
-		#s'il appuie sur la touche 'Echape'
-		elif event.key == K_ESCAPE:
-			game_continue = False
+	#si l'utilisateur appuie sut la touche 'Droite'
+	if touche_enfoncee(clavier, TOUCHE_DROITE):
+		#fixe la direction du perso a la valeur de 'DROITE'
+		perso[2] = DROITE
+		#autorise le deplacement
+		deplace = True
+		
+	#s'il appuie sur la touche 'Gauche'
+	elif touche_enfoncee(clavier, TOUCHE_GAUCHE):
+		#fixe la direction du perso a la valeur de 'GAUCHE'
+		perso[2] = GAUCHE
+		#autorise le deplacement
+		deplace = True
+		
+	#s'il appuie sur la touche 'Bas'
+	elif touche_enfoncee(clavier, TOUCHE_BAS):
+		#fixe la direction du perso a la valeur de 'BAS'
+		perso[2] = BAS
+		#autorise le deplacement
+		deplace = True
+		
+	#s'il appuie sur la touche 'Haut'
+	elif touche_enfoncee(clavier, TOUCHE_HAUT):
+		#fixe la direction du perso a la valeur de 'HAUT'
+		perso[2] = HAUT
+		#autorise le deplacement
+		deplace = True
+		
+	elif touche_enfoncee(clavier, K_e):
+		perso[:2] = list(fin)
+		
+	#s'il appuie sur la touche 'Echap'
+	elif touche_enfoncee(clavier,TOUCHE_ECHAP):
+		jeu_en_cours = False
+		
+	#si le perso peut se deplacer et qu'il n'y a rien devant lui
+	if deplace and peut_deplacer(carte, perso):
+		#regarde si le personnage va arrivee
+		arrive = va_arriver(perso, fin)
+		#deplace le perso a l'ecran
+		dessiner_mouvement(fenetre, perso, arrive)
+		#deplace le perso sur la carte
+		deplacer_perso(perso)
+		
 	#si le perso est sur la case de fin
 	if est_arrivee(perso,fin):
 		#animation de fin du feu
-		anim_fin(fenetre,carte,perso,fin)
-		game_continue = False
+		anim_fin(fenetre, perso)
+		jeu_en_cours = False
 		pause(.5)
 		
+	#si le temps depasse 20 secondes on dit que le joueur a perdu
+	if temps >= 20:
+		#recupere la taille du perso
+		taille_fenetre = obtenir_taille(fenetre)
+		#ecrit 'tu as perdu'
+		ecrire_centre("Tu as perdu!", police_basique, rouge, taille_fenetre)
+		#dessine et affiche tout a l'ecran
+		dessiner_perso(fenetre, perso)
+		#un dernier pour la route
+		tout_dessiner(fenetre)
+		#et on affiche!!
+		afficher_ecran()
+
+		#petite pause avant de quitter le jeu, pour que le joueur puisse voir le resultat
+		pause(1)
+		#pour qutter le jeu
+		jeu_en_cours = False
+		
+		
+	#dessine et affiche tout a l'ecran
+	dessiner_perso(fenetre, perso)
+	#tout_dessiner(fenetre)
+	afficher_ecran()
+
 #quitte le programme
 quitter()
