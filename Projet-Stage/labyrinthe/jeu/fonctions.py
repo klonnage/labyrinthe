6d7afd32392a761c05518b62e9e5b10e61aa31d2
klonnage <vinc.brdionneau@gmail.com>
2015-05-02 18:55:48 +0200

    changement du systeme d'ouverture de carte.

--- Projet-Stage/labyrinthe/jeu/fonctions.py
+++ Projet-Stage/labyrinthe/jeu/fonctions.py
@@ -3,38 +3,26 @@ from constantes import *
 import pygame, json
 
 #initialise le jeu
-def creer_carte():
-	carte = [[1, 1, 1, 1, 1, 1],
-			 [1, 0, 0, 0, 0, 1],
-			 [1, 1, 1, 1, 0, 1],
-			 [1, 0, 0, 0, 0, 1],
-			 [1, 0, 1, 1, 1, 1],
-			 [1, 0, 0, 0, 0, 1],
-			 [1, 1, 1, 1, 1, 1]]
-	return carte
-			 
-#initialise le perso
-def creer_perso():
-	perso = [1, 1, DROITE]
-	return perso
-	
-#initialise la fin	
-def creer_fin():
-	fin = [5, 4]
-	return fin
+def creer_jeu():
+	#ligne a modifier pour chaner la carte
+	nom_fichier = 'carte.json'
+	with open(CHEMIN + nom_fichier, "r") as fichier:
+		donnees = json.load(fichier)
+		carte = donnees["cases"]
+		fin = donnees["fin"]
+		perso = donnees["perso"]
+	return carte, perso, fin
 	
 #deplace le perso sur la carte si le deplacement est possible
-def deplacer_perso(carte, perso):
-	#si on peut se deplacer
-	if not peut_deplacer(carte,perso):
-		if perso[2] == DROITE:
-			perso[1] += 1
-		elif perso[2] == GAUCHE:
-			perso[1] -= 1
-		elif perso[2] == HAUT:
-			perso[0] -= 1
-		elif perso[2] == BAS:
-			perso[0] += 1
+def deplacer_perso(perso):
+	if perso[2] == DROITE:
+		perso[1] += 1
+	elif perso[2] == GAUCHE:
+		perso[1] -= 1
+	elif perso[2] == HAUT:
+		perso[0] -= 1
+	elif perso[2] == BAS:
+		perso[0] += 1
 
 #dessine simplement la carte
 def dessiner_carte(fenetre,carte,fin):
@@ -44,32 +32,34 @@ def dessiner_carte(fenetre,carte,fin):
 		for j,colone in enumerate(ligne):
 			#si la case a dessiner est un sol
 			if colone == 0:
-				dessiner_ecran(fenetre, SOLS,(j*LARGEUR,i*LARGEUR),SOL)
+				ajouter_dessin(SOLS,(j*LARGEUR,i*LARGEUR),SOL)
 			#si la case a dessiner est un mur
 			elif colone == 1:
-				dessiner_ecran(fenetre, SOLS,(j*LARGEUR,i*LARGEUR),MUR)
+				ajouter_dessin(SOLS,(j*LARGEUR,i*LARGEUR),MUR)
 				
 	#dessine la fin du niveau
-	dessiner_ecran(fenetre, SOLS, (fin[1]*LARGEUR, fin[0]*LARGEUR), FIN)
+	ajouter_dessin(SOLS, (fin[1]*LARGEUR, fin[0]*LARGEUR), FIN)
 
 #dessine la carte, le personnage et la case de fin
-def dessiner_tout(fenetre,carte,perso,fin):
-	#dessine la carte et la fin a l'ecran
-	dessiner_carte(fenetre,carte,fin)
-	
+def dessiner_perso(fenetre, perso):
+	#stock temporairement la position du personnage
+	position = (perso[1]*LARGEUR, perso[0]*LARGEUR)
+
 	#dessine le personnage dans la bonne position sur la fenetre
-	dessiner_ecran(fenetre, PERSONNAGE, (perso[1]*LARGEUR, perso[0]*LARGEUR), obtenir_portion_perso(perso[2]))
-					
-	#affiche la fenetre pour que l'utilisateur puisse voir 
-	#l'image
-	afficher_ecran()
+	ajouter_dessin(PERSONNAGE, position, obtenir_portion(perso))
+	
+	#dessine du sol a l'endroit ou est le personnage
+	ajouter_dessin(SOLS, position, SOL)
+	
+	#dessine tout
+	tout_dessiner(fenetre)
 
 #Verifie si le personnage est sur la case de fin		
 def est_arrivee(perso,fin):
 	return perso[0] == fin[0] and perso[1] == fin[1]
 
 #dessine une petite animation pour la fin du niveau
-def anim_fin(fenetre,carte,perso,fin):
+def anim_fin(fenetre, perso):
 	#fait tourner le personnage sur lui meme et affiche
 	#'Tu as gagne'
 	
@@ -78,35 +68,126 @@ def anim_fin(fenetre,carte,perso,fin):
 	police = creer_police(CHEMIN + 'police.ttf', 50)
 	#Le message a afficher
 	message = creer_message('You win!', police)
-	#affiche la carte
-	dessiner_tout(fenetre, carte, perso, fin)
-	afficher_ecran()
-	for i in range(10):
+	#La taille de la fenetre
+	taille_fenetre = obtenir_taille(fenetre)
+	#La taille du message
+	taille_message = obtenir_taille(message)
+	#La position pour que le message soit centre
+	centre = (taille_fenetre[0]-taille_message[0])/2, (taille_fenetre[1]-taille_message[1])/2
+	for etape in range(10):
 		#l'effet pour que le personnage tourne sur lui-meme
-		perso[2] = i % (HAUT + 1)
+		perso[2] = etape % (HAUT + 1)
 		#dessine le perso et la fin
 		position = (perso[1] * LARGEUR, perso[0] * LARGEUR)
-		dessiner_ecran(fenetre, SOLS, position, FIN)
+		ajouter_dessin(SOLS, position, FIN)
 		
-		dessiner_ecran(fenetre, PERSONNAGE, position, obtenir_portion_perso(perso[2]))
+		ajouter_dessin(PERSONNAGE, position, obtenir_portion(perso))
 		
 		#affiche le message
-		fenetre.blit(message, (75, 150))
-		#actualise l'ecran pour que l'utilisateur puisse voir
-		#le rendu
+		ajouter_dessin(message, centre)
+		#dessine et affiche a l'ecran pour que
+		#l'utilisateur puisse voir le rendu
+		tout_dessiner(fenetre)
 		afficher_ecran()
 		#fait une pause pour laisser le temps a l'utilisateur
 		#de voir le resultat
-		pause(.1)
+		pause(.05)
+
+#dessine le personnage en mouvement
+def dessiner_etape_mouvement(fenetre, perso, arrive, etape):
+	#on stock la position de la case sur laquelle est le perso
+	position = (perso[1] * LARGEUR, perso[0] * LARGEUR)
+	#ajoute le sol a la liste des cases a affichee
+	ajouter_dessin(SOLS, position, SOL)
+	if perso[2] == DROITE:
+		#position de la case a droite
+		position = ((perso[1] + 1) * LARGEUR, perso[0] * LARGEUR)
+		#indique la position du personnage
+		deplacement_colone = 1
+		deplacement_ligne = 0
+	elif perso[2] == GAUCHE:
+		#position de la case a droite
+		position = ((perso[1] - 1) * LARGEUR, perso[0] * LARGEUR)
+		#indique la position du personnage
+		deplacement_colone = -1
+		deplacement_ligne = 0
+	elif perso[2] == BAS:
+		#position de la case a droite
+		position = (perso[1] * LARGEUR, (perso[0] + 1) * LARGEUR)
+		#indique la position du personnage
+		deplacement_colone = 0
+		deplacement_ligne = 1
+	elif perso[2] == HAUT:
+		#position de la case a droite
+		position = (perso[1] * LARGEUR, (perso[0] - 1) * LARGEUR)
+		#indique la position du personnage
+		deplacement_colone = 0
+		deplacement_ligne = -1
+		
+	#calcule la position du personnage
+	position_perso = (perso[1] * LARGEUR + deplacement_colone * etape * 25,perso[0] * LARGEUR + deplacement_ligne * etape * 25)
+	#ajoute le personnage dans la liste des images a afficher
+	ajouter_dessin(PERSONNAGE,position_perso,obtenir_portion(perso,(etape + 2) % 3))
+	
+	#si le personnage va arriver
+	if arrive:
+		#ajoute le sol a la liste des images a affichee
+		ajouter_dessin(SOLS, position, FIN)
+	else:
+		#ajoute le sol a la liste des images a affichee
+		ajouter_dessin(SOLS, position, SOL)
+
+#dessine le mouvement du personnage
+def dessiner_mouvement(fenetre, perso, arrive):
+	for etape in range(2):
+		dessiner_etape_mouvement(fenetre, perso, arrive, etape)
+		tout_dessiner(fenetre)
+		afficher_ecran()
+		pause(.01)
+	#dessine un carre de sol a l'acienne position du personnage
+	#qui n'a pas encore ete change
+	position = (perso[1] * LARGEUR, perso[0] * LARGEUR)
+	if arrive:
+		ajouter_dessin(SOLS, position, FIN)
+	else:
+		ajouter_dessin(SOLS, position, SOL)
 		
 #verifie si il y a quelque chose dans la case la plus proche
 #de 'perso' en direction de 'perso[2]'
 def peut_deplacer(carte,perso):
 	if perso[2] == DROITE:
-		return carte[perso[0]][perso[1] + 1] == 1
+		return carte[perso[0]][perso[1] + 1] != 1
 	elif perso[2] == GAUCHE:
-		return carte[perso[0]][perso[1] - 1] == 1
+		return carte[perso[0]][perso[1] - 1] != 1
 	elif perso[2] == BAS:
-		return carte[perso[0] + 1][perso[1]] == 1
+		return carte[perso[0] + 1][perso[1]] != 1
 	elif perso[2] == HAUT:
-		return carte[perso[0] - 1][perso[1]] == 1
+		return carte[perso[0] - 1][perso[1]] != 1
+		
+#indique si le personnage va arriver sur la case d'arrive
+def va_arriver(perso, fin):
+	if perso[2] == DROITE:
+		return perso[1] == fin[1] and perso[0] + 1 == fin[0]
+	elif perso[2] == GAUCHE:
+		return perso[1] == fin[1] and perso[0] - 1 == fin[0]
+	elif perso[2] == BAS:
+		return perso[1] + 1 == fin[1] and perso[0] == fin[0]
+	elif perso[2] == HAUT:
+		return perso[1] - 1 == fin[1] and perso[0] == fin[0]
+		
+#affiche le timer
+def afficher_temps(temps, police, couleur):
+	#creer le message contenant le temps a afficher
+	temps_message = creer_message(str(temps), police, couleur)
+	#affiche deux murs pour afficher le timer par dessus
+	#ajouter_dessin(SOLS, (0, 0), MUR)
+	#ajouter_dessin(SOLS, (0, 50), MUR)
+	
+	#cree le fond
+	fond = creer_surface(100, 50)
+	dessiner_surface(fond, SOLS, (0,0), MUR)
+	dessiner_surface(fond, SOLS, (50,0), MUR)
+	dessiner_surface(fond, temps_message, (10 , 2))
+	
+	#affiche le timer en heut a gauche
+	ajouter_dessin(fond, (0, 0))
